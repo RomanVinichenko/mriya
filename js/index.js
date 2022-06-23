4f1701e682e817ba074f41b12bd367e0ec14e484
RomanVinichenko <chicagezzz@gmail.com>
2022-06-23 12:17:11 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-$(function () {});
+function homeHoverIn() {
+  document.querySelector('.home__arrow').style.cssText = `
+  margin-left: 20px;`
+}
+function homeHoverOut() {
+  document.querySelector('.home__arrow').style.cssText = `
+  margin-left: 10px;`
+}

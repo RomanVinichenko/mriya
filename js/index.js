b45a28a379550e9dbc4c399a0b7e5d6e6716d72f
RomanVinichenko <chicagezzz@gmail.com>
2022-06-24 21:18:48 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -6,3 +6,10 @@ function homeHoverOut() {
   document.querySelector('.home__arrow').style.cssText = `
   margin-left: 10px;`
 }
+
+const btn = document.querySelector('.menu__burger')
+const menu = document.querySelector('.menu__list')
+
+btn.addEventListener('click', function () {
+  menu.classList.toggle('menu__list--active')
+})

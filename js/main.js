10068d897b6431dee138b57251840097bae13d3b
RomanVinichenko <chicagezzz@gmail.com>
2022-06-27 17:19:45 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -20,3 +20,28 @@ const popupClose = document.querySelector('.popup__close')
 popupClose.addEventListener('click', function () {
   popup.classList.remove('popup--active')
 })
+
+function offerCanIn() {
+  document.querySelector('.offer__cannabis').style.cssText = `
+  transform: scale(1.05);`
+}
+function offerCanOut() {
+  document.querySelector('.offer__cannabis').style.cssText = `
+  transform: scale(1.0);`
+}
+function offerMerchIn() {
+  document.querySelector('.offer__merch').style.cssText = `
+  transform: scale(1.05);`
+}
+function offerMerchOut() {
+  document.querySelector('.offer__merch').style.cssText = `
+  transform: scale(1.0);`
+}
+function offerArtIn() {
+  document.querySelector('.offer__art').style.cssText = `
+  transform: scale(1.05);`
+}
+function offerArtOut() {
+  document.querySelector('.offer__art').style.cssText = `
+  transform: scale(1.0);`
+}

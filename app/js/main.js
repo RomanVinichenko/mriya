91f0240d7c13f19cc4c6fe7f58e7b5b52e1aa8ba
RomanVinichenko <chicagezzz@gmail.com>
2022-06-29 10:31:37 +0300

    js edit

--- app/js/main.js
+++ app/js/main.js
@@ -15,27 +15,11 @@ btn.addEventListener('click', function () {
   btn.classList.toggle('menu__burger--active')
 })
 
-function offerCanIn() {
-  document.querySelector('.offer__cannabis').style.cssText = `
-  transform: scale(1.05);`
+function offerZoomIn(x) {
+  document.getElementById("offer-img-" + x).style.cssText =
+    `transform: scale(1.05);`
 }
-function offerCanOut() {
-  document.querySelector('.offer__cannabis').style.cssText = `
-  transform: scale(1.0);`
-}
-function offerMerchIn() {
-  document.querySelector('.offer__merch').style.cssText = `
-  transform: scale(1.05);`
-}
-function offerMerchOut() {
-  document.querySelector('.offer__merch').style.cssText = `
-  transform: scale(1.0);`
-}
-function offerArtIn() {
-  document.querySelector('.offer__art').style.cssText = `
-  transform: scale(1.05);`
-}
-function offerArtOut() {
-  document.querySelector('.offer__art').style.cssText = `
-  transform: scale(1.0);`
+function offerZoomOut(x) {
+  document.getElementById("offer-img-" + x).style.cssText =
+    `transform: scale(1.0);`
 }
